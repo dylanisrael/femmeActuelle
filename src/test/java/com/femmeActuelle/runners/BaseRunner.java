97ab097a4c26468b3c140e4b2258742c25bb5199
dylanisrael <israelmink98@gmail.com>
2021-08-24 14:09:11 +0100

    checkout

--- src/test/java/com/femmeActuelle/runners/BaseRunner.java
+++ src/test/java/com/femmeActuelle/runners/BaseRunner.java
@@ -2,30 +2,28 @@ package com.femmeActuelle.runners;
 
 import com.femmeActuelle.config.Properties;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseRunner extends AbstractTestNGCucumberTests {
-
-    private static final Logger Log =  LogManager.getLogger( BaseRunner.class);
 
 
     @Parameters({"browser", "device"})
     @BeforeClass
     public static void beforeTest(@Optional String browser, @Optional String device){
         String browserA;
+
         browserA = java.util.Optional
                 .ofNullable(browser)
                 .orElse(Properties.Config.getBrowser());
-        Log.info("Tests are starting");
-        Properties.DriverManager.setDriver(browserA);
 
+        Properties.DriverManager.setDriver(browserA);
     }
 
-    @AfterTest
+    @AfterClass
     public static void tearDown(){
-        Log.info("Tests are ending");
         Properties.DriverManager.getDriver().quit();
     }
 
